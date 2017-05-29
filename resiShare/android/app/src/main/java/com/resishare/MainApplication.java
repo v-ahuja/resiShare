08fc70fd28ab46473a22093cd9a8b95e1e2101ca
vahuja <vineet.ahuja1990@gmail.com>
2017-05-29 17:27:53 -0400

    Attempt react upgrade

--- resiShare/android/app/src/main/java/com/resishare/MainApplication.java
+++ resiShare/android/app/src/main/java/com/resishare/MainApplication.java
@@ -1,12 +1,10 @@
 package com.resishare;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import io.fullstack.firestack.FirestackPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
